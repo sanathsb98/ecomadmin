eb83efa20293050a74bfdc8f1c6eabad0a8896ce
sanathsb98 <itssanathsb@gmail.com>
2025-01-29 15:40:37 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import Dashboard from './pages/dashboard/Dashboard';
 import MainPage from './pages/mainpage/MainPage';
 import Orders from './pages/orders/Orders';
 import Products from './pages/products/Products';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
   <>
-    <Navbar />
-    <MainPage/>
+   <Navbar />
+   <MainPage/>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/products' element={<Products/>}/>
+  </Routes>
+  </BrowserRouter>
   </>
       
       
